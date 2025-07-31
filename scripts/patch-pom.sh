35fe8a60f1bfc5a6f4f331c6157642f4f59a0202
viscontm <maxime.visconte@ifpen.fr>
2025-07-31 14:29:42 +0200

    Ajout patch pom pour publication maven central

--- scripts/patch-pom.sh
+++ scripts/patch-pom.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
-set -e
 
-POM="target/generated-sources/java/pom.xml"
+POM_FILE="target/generated-sources/java/pom.xml"
 
-if [ ! -f "$POM" ]; then
-  echo "Erreur : $POM non trouvé"
+PLUGIN_XML='
+<plugin>
+  <groupId>org.sonatype.central</groupId>
+  <artifactId>central-publishing-maven-plugin</artifactId>
+  <version>0.7.0</version>
+  <extensions>true</extensions>
+  <configuration>
+    <publishingServerId>central</publishingServerId>
+    <autoPublish>true</autoPublish>
+  </configuration>
+</plugin>
+'
+
+# Vérifie que xmlstarlet est installé
+if ! command -v xmlstarlet &> /dev/null; then
+  echo "Erreur: xmlstarlet doit être installé pour exécuter ce script."
   exit 1
 fi
 
-# Vérifier si distributionManagement est déjà présent
-if grep -q "<distributionManagement>" "$POM"; then
-  echo "distributionManagement déjà présent dans $POM, rien à faire."
-  exit 0
+# Vérifie que pom.xml existe
+if [ ! -f "$POM_FILE" ]; then
+  echo "Erreur: $POM_FILE non trouvé."
+  exit 1
 fi
 
-# Créer un fichier temporaire
-TMP_FILE=$(mktemp)
+# Ajoute la balise <plugins> si elle n'existe pas dans <build>
+xmlstarlet ed -L \
+  -i "/project/build[not(plugins)]" -t elem -n plugins -v "" \
+  "$POM_FILE"
 
-# Copier tout sauf la dernière ligne (</project>)
-head -n -1 "$POM" > "$TMP_FILE"
-
-# Ajouter le bloc distributionManagement
-cat >> "$TMP_FILE" <<EOF
-  <distributionManagement>
-    <repository>
-      <id>ossrh</id>
-      <name>OSSRH Repository</name>
-      <url>https://oss.sonatype.org/service/local/staging/deploy/maven2/</url>
-    </repository>
-    <snapshotRepository>
-      <id>ossrh-snapshots</id>
-      <name>OSSRH Snapshots Repository</name>
-      <url>https://oss.sonatype.org/content/repositories/snapshots/</url>
-    </snapshotRepository>
-  </distributionManagement>
-EOF
-
-# Réajouter la balise fermante </project>
-echo "</project>" >> "$TMP_FILE"
-
-# Remplacer l'ancien POM
-mv "$TMP_FILE" "$POM"
-
-echo "✅ Section distributionManagement injectée proprement dans $POM"
+# Ajoute le plugin dans <plugins> s'il n'existe pas déjà (par artifactId)
+if ! xmlstarlet sel -t -v "count(/project/build/plugins/plugin[artifactId='central-publishing-maven-plugin'])" "$POM_FILE" | grep -q '^1$'; then
+  # Ajout du plugin à la fin de plugins
+  xmlstarlet ed -L -s "/project/build/plugins" -t elem -n pluginTMP -v "" \
+    -s "/project/build/plugins/pluginTMP" -t elem -n groupId -v "org.sonatype.central" \
+    -s "/project/build/plugins/pluginTMP" -t elem -n artifactId -v "central-publishing-maven-plugin" \
+    -s "/project/build/plugins/pluginTMP" -t elem -n version -v "0.7.0" \
+    -s "/project/build/plugins/pluginTMP" -t elem -n extensions -v "true" \
+    -s "/project/build/plugins/pluginTMP" -t elem -n configuration -v "" \
+    -s "/project/build/plugins/pluginTMP/configuration" -t elem -n publishingServerId -v "central" \
+    -s "/project/build/plugins/pluginTMP/configuration" -t elem -n autoPublish -v "true" \
+    -r "/project/build/plugins/pluginTMP" -v plugin \
+    "$POM_FILE"
+  echo "Plugin ajouté au pom.xml"
+else
+  echo "Plugin déjà présent dans pom.xml"
+fi
